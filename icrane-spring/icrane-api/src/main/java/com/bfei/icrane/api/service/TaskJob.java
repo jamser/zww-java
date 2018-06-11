319e8c7690faa351c42112f094e3a3b1133b98de
bruce <1050120599@qq.com>
2018-06-11 14:31:31 +0800

    修改定时任务时间

--- icrane-spring/icrane-api/src/main/java/com/bfei/icrane/api/service/TaskJob.java
+++ icrane-spring/icrane-api/src/main/java/com/bfei/icrane/api/service/TaskJob.java
@@ -1,10 +1,13 @@
 package com.bfei.icrane.api.service;
 
+import com.bfei.icrane.api.controller.AgentBankController;
+import com.bfei.icrane.common.util.Enviroment;
 import com.bfei.icrane.common.util.RedisKeyGenerator;
 import com.bfei.icrane.common.util.RedisUtil;
 import com.bfei.icrane.common.util.TimeUtil;
 import com.bfei.icrane.core.models.*;
 import com.bfei.icrane.core.service.AccountService;
+import com.bfei.icrane.core.service.AgentChargeService;
 import com.bfei.icrane.core.service.DollService;
 import com.bfei.icrane.core.service.VipService;
 import org.slf4j.Logger;
@@ -14,6 +17,7 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /**
@@ -37,6 +41,15 @@ public class TaskJob {
     private VipService vipService;
     @Autowired
     private ChargeService chargeService;
+
+    @Autowired
+    private AgentChargeService agentChargeService;
+
+    @Autowired
+    private SystemPrefService systemPrefService;
+
+    @Autowired
+    private AgentService agentService;
 
     /**
      * 兑换每日过期娃娃
@@ -101,5 +114,95 @@ public class TaskJob {
             payingUser.setGrowthValue(leastAllowed);
             accountService.updateMemberGrowthValue(payingUser);
         }
+    }
+
+
+    @Scheduled(cron = "0 0 1 * * ?") //每天一点执行
+//    @Scheduled(cron = "0/5 * *  * * ? ")
+    public void agentIncomeTrans() {
+        List<AgentCharge> chargeList = agentChargeService.selectByStatus(0);
+        Integer time = Integer.valueOf(systemPrefService.selectByPrimaryKey(Enviroment.BALANCE_CHANGE_TIME).getValue());
+        if (time == 0) {
+            return;
+        }
+
+        for (int i = 0; i < chargeList.size(); i++) {
+            Calendar calendar = Calendar.getInstance();
+            AgentCharge agentCharge = chargeList.get(i);
+            int count = time;
+
+            for (int j = 1; j <= count; j++) {
+                calendar.setTime(new Date());
+                calendar.add(Calendar.DAY_OF_MONTH, -j);
+                Date date = calendar.getTime();
+                if (isWeekend(date)) {
+                    count++;
+                }
+            }
+            calendar.setTime(new Date());
+            calendar.add(Calendar.DAY_OF_MONTH, -count);
+            Date oldDate = isThursday(calendar.getTime());
+            logger.info("当前时间==={},订单时间=={},结算时间==={}",
+                    new Date(), agentCharge.getCreateTime(), oldDate);
+
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+            String format = sdf.format(agentCharge.getCreateTime());
+            String format1 = sdf.format(oldDate);
+
+            if (format.compareTo(format1) <= 0) {
+                if (agentCharge.getAgentOneIncome() > 0) {
+                    Agent   agent = new Agent();
+                    agent.setId(agentCharge.getAgentSuperId());
+                    agent.setBalance(agentCharge.getAgentSuperIncome());
+                    agentService.updateAgentBalance(agent);
+                    logger.info("【订单任务】代理商 == {},转入余额 {}", agent.getId(), agent.getBalance());
+                }
+                if (agentCharge.getAgentOneIncome() > 0) {
+                    Agent agent = new Agent();
+                    agent.setId(agentCharge.getAgentOneId());
+                    agent.setBalance(agentCharge.getAgentOneIncome());
+                    agentService.updateAgentBalance(agent);
+                    logger.info("【订单任务】代理商 == {},转入余额 {}", agent.getId(), agent.getBalance());
+                }
+                if (agentCharge.getAgentTwoIncome() > 0) {
+                    Agent   agent = new Agent();
+                    agent.setId(agentCharge.getAgentTwoId());
+                    agent.setBalance(agentCharge.getAgentTwoIncome());
+                    agentService.updateAgentBalance(agent);
+                    logger.info("【订单任务】代理商 == {},转入余额 {}", agent.getId(), agent.getBalance());
+                }
+                if (agentCharge.getAgentThreeIncome() > 0) {
+                    Agent agent = new Agent();
+                    agent.setId(agentCharge.getAgentThreeId());
+                    agent.setBalance(agentCharge.getAgentThreeIncome());
+                    agentService.updateAgentBalance(agent);
+                    logger.info("【订单任务】代理商 == {},转入余额 {}", agent.getId(), agent.getBalance());
+                }
+                int type = agentChargeService.updateStatus(agentCharge.getId());
+                logger.info("【订单任务】代理交易ID === {} 用户订单ID ==== {} ，成功为{}", agentCharge.getId(), agentCharge.getOrderId(), type);
+            }
+
+
+        }
+    }
+
+    public boolean isWeekend(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        if (cal.get(Calendar.DAY_OF_WEEK) == Calendar.SATURDAY || cal.get(Calendar.DAY_OF_WEEK) == Calendar.SUNDAY) {
+            return true;
+        }
+        return false;
+    }
+
+
+    public Date isThursday(Date date) {
+        Calendar cal = Calendar.getInstance();
+        cal.setTime(date);
+        if (cal.get(Calendar.DAY_OF_WEEK) - 1 == 5) {
+            cal.add(Calendar.DAY_OF_MONTH, +2);
+            return cal.getTime();
+        }
+        return date;
     }
 }
